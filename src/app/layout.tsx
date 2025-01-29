107f0da9fe906be5807fbe66794bdacd14143c8d
sebasti810 <spusch@outlook.de>
2025-01-29 09:03:56 +0100

    fix: opengraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
     title: siteConfig.title,
     description: siteConfig.description,
     siteName: siteConfig.title,
-    images: [`${siteConfig.url}/images/og.jpg`],
+    images: [`${siteConfig.url}/images/prism_logo.png`],
     type: 'website',
     locale: 'en_US',
   },
