3a1c8887a916d347372d46828b188a8db684c3f7
sebasti810 <spusch@outlook.de>
2025-02-01 10:10:09 +0100

    refactor: remove video, add transparent gif

--- src/app/components/ContentSections.tsx
+++ src/app/components/ContentSections.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 import HowItWorksSection from '@/app/components/HowDoesItWork';
@@ -6,25 +7,27 @@ import WhyPrismSection from '@/app/components/WhyPrismSection';
 
 const ContentSection = () => {
   return (
-    <section className='min-h-screen px-4 py-16 md:py-24'>
-      <div className='mx-auto max-w-[97%] md:max-w-[90%]'>
+    <section className='min-h-screen px-4 py-16 lg:py-24'>
+      <div className='mx-auto max-w-[97%] lg:max-w-[90%]'>
         <div className='flex flex-col items-center lg:flex-row lg:items-start lg:gap-8'>
           <div className='flex-1'>
-            <h1 className='mb-4 text-center font-advercase text-3xl text-white md:mb-12 md:text-left md:text-6xl'>
+            <h1 className='mb-4 text-center font-advercase text-3xl text-white lg:mb-12 lg:text-left lg:text-6xl'>
               <span className='block'>Making the internet</span>
               <span className='block md:leading-[1.6]'>verifiable, not just secure.</span>
             </h1>
-            <PrismGuideButton />
+            <div className='flex justify-center'>
+              <PrismGuideButton />
+            </div>
           </div>
-          <div className='w-full md:w-[40%]'>
-            <video
-              src='/images/Verif_new3.mp4'
-              autoPlay
-              loop
-              muted
-              playsInline
-              className='w-full rounded-md shadow-md'
-            ></video>
+          <div className='h-[500] w-[500] lg:h-[600] lg:w-[600]'>
+            <Image
+              src='/images/Sequence01.gif'
+              alt='Gif Sequence'
+              width={500}
+              height={500}
+              className='rounded-lg lg:h-[700px] lg:w-[700px]'
+              unoptimized
+            />
           </div>
         </div>
       </div>
